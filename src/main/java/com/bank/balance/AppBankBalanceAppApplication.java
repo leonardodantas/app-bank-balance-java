0740eb5910c1f44c36bcf6bef0bffd4bea2f50d5
Leo Dantas <leonardo1317@yahoo.com.br>
2022-12-13 08:39:33 -0300

    Iniciando projeto

---
+++ src/main/java/com/bank/balance/AppBankBalanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.bank.balance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBankBalanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBankBalanceAppApplication.class, args);
+	}
+
+}
