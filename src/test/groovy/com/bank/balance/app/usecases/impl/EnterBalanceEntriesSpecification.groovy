bc27c5edba4cf4d51b0fb82e9d261806dfc2f29e
Leonardo Dantas <leonardo1317@yahoo.com.br>
2022-12-30 18:46:35 -0300

    Iniciando primeiro teste

--- src/test/groovy/com/bank/balance/app/usecases/impl/EnterBalanceEntriesSpecification.groovy
+++ src/test/groovy/com/bank/balance/app/usecases/impl/EnterBalanceEntriesSpecification.groovy
@@ -2,6 +2,7 @@ package com.bank.balance.app.usecases.impl
 
 import com.bank.balance.app.repositories.ICustomerBalanceRepository
 import com.bank.balance.app.repositories.ITransactionRepository
+import com.bank.balance.domain.CustomerBalance
 import com.bank.balance.domain.UserBalanceEntry
 import com.bank.balance.domain.UsersBalancesEntriesAdapter
 import com.bank.balance.utils.GetMockJson
@@ -16,19 +17,31 @@ class EnterBalanceEntriesSpecification extends Specification {
 
     def getMockJson = new GetMockJson()
 
+    /**
+     * deveExecutarQuatroTransacoesDeUmUnicoUsuario
+     * deve Lançar Exceção quando existir transações repetidas na lista
+     * deve lançar exceções quando alguma transação ja existir no banco de dados
+     */
+
     def "deveExecutarQuatroTransacoesDeUmUnicoUsuario"(){
         given: "um userBalanceEntry valido"
         def userBalanceEntry = getMockJson.execute("usuario-com-quatro-transacoes-validas", UserBalanceEntry.class)
 
-        and: "não existir nenhuma transação do json ja armazenada no banco de dados"
+        and: "quando não existir nenhuma transação do json ja armazenada no banco de dados"
         transactionRepository.findByTransactionsId(_ as List<String>) >> List.of()
 
-        and: ""
+        and: "não existir nenhum usuario com o saldo salvo no banco de dados"
         customerBalanceRepository.findAllById(_ as List<String>) >> List.of()
+
         when: "executar o caso de uso enterBalanceEntries"
+
         def response = enterBalanceEntries.execute(UsersBalancesEntriesAdapter.from(userBalanceEntry))
 
-        then: "ddd"
+        then: "response deve ser diferente de null"
+        assert response != null
+
+        and: "salvar a lista de saldo do usuario uma unica vez"
+        1 * customerBalanceRepository.save(_ as List<CustomerBalance>)
     }
 
 }
