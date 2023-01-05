60a7615e3fdd0bb2433c15125b8645e59ad00889
Leonardo Dantas <leonardo1317@yahoo.com.br>
2023-01-05 08:49:48 -0300

    Criação de novos advices

--- src/main/java/com/bank/balance/infra/http/controllers/AdviceController.java
+++ src/main/java/com/bank/balance/infra/http/controllers/AdviceController.java
@@ -3,6 +3,8 @@ package com.bank.balance.infra.http.controllers;
 import com.bank.balance.app.exceptions.ExistingTransactionsException;
 import com.bank.balance.app.exceptions.TransactionIdFoundException;
 import com.bank.balance.app.exceptions.TransactionTypeInvalidException;
+import com.bank.balance.infra.exceptions.ConvertFileException;
+import com.bank.balance.infra.exceptions.SaveEntityException;
 import com.bank.balance.infra.http.jsons.responses.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -28,4 +30,17 @@ public class AdviceController {
         final var response = ErrorResponse.from(exception.getMessage());
         return ResponseEntity.unprocessableEntity().body(response);
     }
+
+    @ExceptionHandler(ConvertFileException.class)
+    public ResponseEntity<ErrorResponse> handlerConvertFileException(final ConvertFileException exception) {
+        final var response = ErrorResponse.from(exception.getMessage());
+        return ResponseEntity.unprocessableEntity().body(response);
+    }
+
+    @ExceptionHandler(SaveEntityException.class)
+    public ResponseEntity<ErrorResponse> handlerSaveEntityException(final SaveEntityException exception) {
+        final var response = ErrorResponse.from(exception.getMessage());
+        return ResponseEntity.unprocessableEntity().body(response);
+    }
+
 }
