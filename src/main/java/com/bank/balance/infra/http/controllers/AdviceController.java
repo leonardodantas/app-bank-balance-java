0fd624b5b50e5e5d5e8de8ebd5e49ef387420adf
Leonardo Dantas <leonardo1317@yahoo.com.br>
2023-01-05 08:48:02 -0300

    Criação de novos advices

--- src/main/java/com/bank/balance/infra/http/controllers/AdviceController.java
+++ src/main/java/com/bank/balance/infra/http/controllers/AdviceController.java
@@ -1,6 +1,8 @@
 package com.bank.balance.infra.http.controllers;
 
+import com.bank.balance.app.exceptions.ExistingTransactionsException;
 import com.bank.balance.app.exceptions.TransactionIdFoundException;
+import com.bank.balance.app.exceptions.TransactionTypeInvalidException;
 import com.bank.balance.infra.http.jsons.responses.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,7 +12,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class AdviceController {
 
     @ExceptionHandler(TransactionIdFoundException.class)
-    public ResponseEntity<ErrorResponse> handlerAlreadyUserException(final TransactionIdFoundException exception) {
+    public ResponseEntity<ErrorResponse> handlerTransactionIdFoundException(final TransactionIdFoundException exception) {
+        final var response = ErrorResponse.from(exception.getMessage());
+        return ResponseEntity.unprocessableEntity().body(response);
+    }
+
+    @ExceptionHandler(TransactionTypeInvalidException.class)
+    public ResponseEntity<ErrorResponse> handlerTransactionTypeInvalidException(final TransactionTypeInvalidException exception) {
+        final var response = ErrorResponse.from(exception.getMessage());
+        return ResponseEntity.unprocessableEntity().body(response);
+    }
+
+    @ExceptionHandler(ExistingTransactionsException.class)
+    public ResponseEntity<ErrorResponse> handlerExistingTransactionsException(final ExistingTransactionsException exception) {
         final var response = ErrorResponse.from(exception.getMessage());
         return ResponseEntity.unprocessableEntity().body(response);
     }
